fa496e656e1acb6069e1deeb74e49c2a1a886e72
sathivel <sathivel@Stanleys-MacBook-Pro.local>
2022-04-20 23:55:21 +0800

    Test script has changed to feature

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -3,4 +3,4 @@ php /var/www/secretlab/artisan migrate --force
 php /var/www/secretlab/artisan config:cache
 php /var/www/secretlab/artisan config:clear
 cd /var/www/secretlab
-./vendor/bin/phpunit
+./vendor/bin/phpunit --testsuite=Feature
